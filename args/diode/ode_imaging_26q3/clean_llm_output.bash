c66db5594de6b76729ab61c0ad5adf813418ccda
rafelafrance <raphael.lafrance@gmail.com>
2026-08-11 16:35:25 -0400

    Correct log files for data cleaning script

--- args/diode/ode_imaging_26q3/clean_llm_output.bash
+++ args/diode/ode_imaging_26q3/clean_llm_output.bash
@@ -5,18 +5,18 @@
 #     --parsed-file "$data_dir"/parse_text_260809.csv \
 #     --clean-file "$data_dir"/clean_text_260811.csv \
 #     --prompt prompts/diode_v2.md \
-#     --log-file "$data_dir"/parse_text.log
+#     --log-file "$data_dir"/clean_text.log
 
 # data_dir=data/diode/fsca
 # uv run llama/clean_llm_output.py \
 #     --parsed-file "$data_dir"/parse_text_260810.csv \
 #     --clean-file "$data_dir"/clean_text_260811.csv \
 #     --prompt prompts/diode_v2.md \
-#     --log-file "$data_dir"/parse_text.log
+#     --log-file "$data_dir"/clean_text.log
 
 data_dir=data/diode/amnh
 uv run llama/clean_llm_output.py \
     --parsed-file "$data_dir"/parse_text_260810.csv \
     --clean-file "$data_dir"/clean_text_260811.csv \
     --prompt prompts/diode_v2.md \
-    --log-file "$data_dir"/parse_text.log
+    --log-file "$data_dir"/clean_text.log
