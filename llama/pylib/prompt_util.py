cd381a8a67f5d78c28e127c1adddddb2411c0df0
rafelafrance <raphael.lafrance@gmail.com>
2026-08-14 08:50:38 -0400

    Add new files

---
+++ llama/pylib/prompt_util.py
@@ -0,0 +1,17 @@
+import re
+from typing import TYPE_CHECKING
+
+import yaml
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def get_front_yaml(text: str, path: Path) -> dict:
+    top = re.search("^---$.*^---$", text, flags=re.MULTILINE | re.DOTALL)
+    if not top:
+        raise ValueError(f"Improperly formatted prompt file. {path}")
+
+    top = top.group(0).replace("---", "")
+    front = yaml.safe_load(top)
+    return front
