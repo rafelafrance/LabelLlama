0d6a0eda6956616435dd44a7480598937121d2c5
rafelafrance <raphael.lafrance@gmail.com>
2026-08-31 18:16:48 -0400

    Remove unneeded post_with_retries function

--- args/diode/ode_imaging_26q3/amnh_pipeline.bash
+++ args/diode/ode_imaging_26q3/amnh_pipeline.bash
@@ -16,21 +16,22 @@ uv run ./llama/ocr_images.py \
   --notes "More AMNH diode images." \
   --log-file "$data_dir"/ocr_imaging.log
 
-# uv run llama/parse_text.py \
-#     --ocr-file "$data_dir"/ocr_images_260827.csv \
-#     --parsed-file "$data_dir"/parse_text_260827.csv \
-#     --prompt prompts/diode_v2.md \
-#     --model-id Qwen3.6-35B-A3B-UD-Q8_K_XL \
-#     --api-host "http://localhost:9931/v1" \
-#     --temperature 0.1 \
-#     --timeout 300 \
-#     --threads 4 \
-#     --notes "More AMNH diode images." \
-#     --log-file "$data_dir"/parse_text.log
+uv run llama/parse_text.py \
+    --ocr-file "$data_dir"/ocr_images_"$stamp".csv \
+    --parsed-file "$data_dir"/parse_text_"$stamp".csv \
+    --prompt prompts/diode_v2.md \
+    --model-id unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q4_K_XL \
+    --api-host "http://localhost:9931/v1" \
+    --max-tokens 32768 \
+    --temperature 0.1 \
+    --timeout 300 \
+    --threads 4 \
+    --notes "More AMNH diode images." \
+    --log-file "$data_dir"/parse_text.log
 
-# uv run llama/clean_text.py \
-#     --parsed-file "$data_dir"/parse_text_260827.csv \
-#     --clean-file "$data_dir"/clean_text_260827.csv \
-#     --prompt prompts/diode_v2.md \
-#     --notes "More AMNH diode images." \
-#     --log-file "$data_dir"/clean_text.log
+uv run llama/clean_text.py \
+    --parsed-file "$data_dir"/parse_text_260831.csv \
+    --clean-file "$data_dir"/clean_text_260831.csv \
+    --prompt prompts/diode_v2.md \
+    --notes "More AMNH diode images." \
+    --log-file "$data_dir"/clean_text.log
